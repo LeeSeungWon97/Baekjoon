94edbf103d16b05ffd38b13f64cb832fa3b9475f
LeeSeungWon97 <tmddnjs1122@naver.com>
2022-08-30 16:41:44 +0900

    Step6_1
    6.문자열 - 11654번, 11720번

---
+++ Baekjoon/src/step6/P_01.java
@@ -0,0 +1,21 @@
+/* 문자열 */
+
+// 11654번
+
+package step6;
+
+import java.util.Scanner;
+
+public class P_01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		char ch = scan.next().charAt(0);
+		int asc = (int) ch;
+
+		System.out.println(asc);
+
+	}
+
+}
---
+++ Baekjoon/src/step6/P_02.java
@@ -0,0 +1,30 @@
+// 11720번
+
+package step6;
+
+import java.util.Scanner;
+
+public class P_02 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		// 입력할 숫자의 자릿 수
+		int N = scan.nextInt();
+
+		// 입력할 숫자
+		String num = scan.next();
+
+		// 입력한 숫자의 각자릿수 합
+		int sum = 0;
+
+		for (int i = 0; i < N; i++) {
+			sum += num.charAt(i) - '0'; // ASCII 코드 활용
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
